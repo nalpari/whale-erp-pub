10a66575f51accfd9c725304b4f95722ae1e31bd
김창수 <chang9811@interplug.co.kr>
2025-10-17 18:26:35 +0900

    style: layout  기초 작업 추가

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,15 +1,15 @@
-import { ReactNode } from "react";
-import Lnb from "@/components/ui/common/Lnb";
+import { ReactNode } from 'react'
+import Lnb from '@/components/ui/common/Lnb'
 
 interface MainLayoutProps {
   children: ReactNode
 }
-export default function MainLayout({children}: MainLayoutProps){
-  return(
+export default function MainLayout({ children }: MainLayoutProps) {
+  return (
     <div className="wrap">
       <Lnb />
       <div className="container">
-        {children}
+        <div className="frame">{children}</div>
       </div>
     </div>
   )
--- src/components/ui/common/Lnb.tsx
+++ src/components/ui/common/Lnb.tsx
@@ -1,7 +1,48 @@
+'use client'
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function Lnb() {
   return (
-    <div>
-      <h1>Lnb</h1>
+    <div className="lnb">
+      <button className="lnb-toggle-btn"></button>
+      <div className="lnb-header">
+        <Link href="/" className="lnb-logo">
+          <Image src="/assets/images/ui/lnb_logo.svg" alt="logo" fill />
+          <div className="lnb-logo-text">
+            <span className="logo-main">whale ERP</span>
+            <span>management System</span>
+          </div>
+        </Link>
+      </div>
+      <div className="lnb-menu-info">
+        <div className="lnb-menu-icon">
+          <Image src="/assets/images/ui/lnb_menu_img01.svg" alt="menu" fill />
+        </div>
+        <div className="lnb_menu_name">서비스 네이밍</div>
+      </div>
+      <div className="lnb-body">
+        <ul className="lnb-list">
+          <li className="lnb-item">
+            <Link href="#" className="menu-depth01" onClick={(e) => e.preventDefault()}>
+              <Image src="/assets/images/ui/lnb_menu_icon02.svg" alt="menu" fill />
+              <span className="lnb-menu-name">Master data 관리</span>
+            </Link>
+            <ul className="lnb-list-depth02">
+              <li className="lnb-depth02-item">
+                <Link href="#" className="menu-depth02">
+                  <span className="lnb-menu-name">Business Partner Master</span>
+                </Link>
+              </li>
+              <li className="lnb-depth02-item">
+                <Link href="#" className="menu-depth02">
+                  <span className="lnb-menu-name">회원 Master</span>
+                </Link>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
