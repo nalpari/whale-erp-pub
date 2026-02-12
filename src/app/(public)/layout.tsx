47859d391c411ac975bf725f898d2ee66e9f34fc
김창수 <chang9811@interplug.co.kr>
2026-02-12 16:13:55 +0900

    style: 로그인 전 footer 추가

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,9 +1,12 @@
 import BeforeHeader from '@/components/ui/common/BeforeHeader'
+import BeforeFooter from '@/components/ui/common/BeforeFooter'
+
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="main-wrap">
       <BeforeHeader />
       <div className="container">{children}</div>
+      <BeforeFooter />
     </div>
   )
 }
---
+++ src/components/ui/common/BeforeFooter.tsx
@@ -0,0 +1,30 @@
+export default function BeforeFooter() {
+  return (
+    <div className="before-footer">
+      <div className="before-footer-inner">
+        <div className="footer-info-wrap">
+          <div className="footer-icon"></div>
+          <div className="footer-info">
+            <ul className="footer-info-list">
+              <li className="footer-info-item">이용약관</li>
+              <li className="footer-info-item">개인정보처리방침</li>
+            </ul>
+            <ul className="footer-info-list">
+              <li className="footer-info-item">상호: 인터플러그</li>
+              <li className="footer-info-item">주소: 03787 서울특별시 서대문구 연세로 5다길 22-3, 발리빌딩 3층</li>
+            </ul>
+            <ul className="footer-info-list">
+              <li className="footer-info-item">이메일: help@interplug.co.kr</li>
+              <li className="footer-info-item">전화번호: 6923-0028</li>
+              <li className="footer-info-item">사업자등록번호 : 1233467890</li>
+            </ul>
+            <ul className="footer-info-list">
+              <li className="footer-info-item">통신판매업신고번호: 2021-00000000000</li>
+            </ul>
+            <div className="copyright">Copyrights© 2026 INTERPLUG. All Rights Reserved.</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
