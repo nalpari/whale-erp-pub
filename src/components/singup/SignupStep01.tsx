634793901b0fceb0019468120d69f7ee122c3a98
김창수 <chang9811@interplug.co.kr>
2026-03-09 15:49:11 +0900

    style: 회원가입 등록번호 필드 추가

--- src/components/singup/SignupStep01.tsx
+++ src/components/singup/SignupStep01.tsx
@@ -10,19 +10,42 @@ export default function SignupStep01({ setSignupStep }: { setSignupStep: (step: 
           사업자등록번호로 가입하기
         </button>
         <button className={`signup-step01-tab-btn ${step === 2 ? 'act' : ''}`} onClick={() => setStep(2)}>
-          초대 이메일로 가입하기{' '}
+          초대 이메일로 가입하기
         </button>
       </div>
       {step === 1 ? (
         <div className="signup-step01-content-wrap">
           <div className="signup-step01-item">
             <div className="signup-step01-item-tit">사업자 등록 정보를 확인합니다.</div>
-            <div className="signup-step01-item-input">
-              <input type="text" className="input-frame" placeholder="사업자등록번호를 입력해주세요." />
+            <div className="signup-step01-item-input-wrap">
+              <div className="signup-step01-item-input-label">
+                대표자 성명 <span className="red">*</span>
+              </div>
+              <div className="signup-step01-item-input">
+                <input type="text" className="input-frame" placeholder="대표자 성명을 입력해주세요." />
+              </div>
+              <div className="signup-message error mt10">대표자 성명을 다시 확인해서 입력해 주세요.</div>
             </div>
-            <div className="signup-error-message">사업자등록번호를 다시 확인해서 입력해 주세요.</div>
+            <div className="signup-step01-item-input-wrap">
+              <div className="signup-step01-item-input-label">
+                개업일자 <span className="red">*</span>
+              </div>
+              <div className="signup-step01-item-input">
+                <input type="text" className="input-frame" placeholder="개업일자를 입력해주세요." />
+              </div>
+              <div className="signup-message error mt10">개업일자를 다시 확인해서 입력해 주세요.</div>
+            </div>
+            <div className="signup-step01-item-input-wrap">
+              <div className="signup-step01-item-input-label">
+                사업자등록번호 <span className="red">*</span>
+              </div>
+              <div className="signup-step01-item-input">
+                <input type="text" className="input-frame" placeholder="사업자등록번호를 입력해주세요." />
+              </div>
+              <div className="signup-message mt10">- 없이 숫자 10자리를 입력해 주세요</div>
+              <div className="signup-message error mt10">사업자등록번호를 다시 확인해서 입력해 주세요.</div>
+            </div>
             <div className="signup-step01-item-desc">
-              <span>- 없이 숫자 10자리를 입력해 주세요</span>
               <span>
                 사업자 정보 확인(인증)에 문제가 있을 경우, 사업자등록증 사본 파일을 첨부하고{' '}
                 <b>master@erpwhale.co.kr</b>로 연락처 및 신청인 정보를 보내주세요.
